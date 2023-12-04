c8e522de047b997a59bd0baa5339ca8a0763a664
Aleksandr Shcherbakov <au@schmail.ru>
2023-12-04 22:52:40 +0300

    add config flags and env

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"log"
+)
+
+func main() {
+
+	log.Println("Starting application initialization...")
+
+	log.Println("Config initializing...")
+	//config := configs.NewConfig()
+}
