d8b37ae4bef1b57711bfe6c65c75312a21703df2
Aleksandr Scherbakov <105049507+Schalure@users.noreply.github.com>
2023-12-06 16:23:54 +0300

    logger level setting added

--- internal/loggers/sloglogger.go
+++ internal/loggers/sloglogger.go
@@ -13,7 +13,22 @@ type Logger struct {
 
 func NewLogger(config *configs.Config) *Logger {
 
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	var loggerLevel slog.Level
+
+	switch config.AppConfig.Env {
+	case configs.Debug:
+		loggerLevel = slog.LevelDebug
+	case configs.Local:
+		loggerLevel = slog.LevelInfo
+	case configs.Prod:
+		loggerLevel = slog.LevelError
+	}
+
+	loggerHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: loggerLevel,
+	})
+
+	logger := slog.New(loggerHandler)
 
 	return &Logger{
 		logger: logger,
