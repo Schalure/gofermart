448c08e10ab5c8f966f522496587a6bde277d088
Aleksandr Scherbakov <105049507+Schalure@users.noreply.github.com>
2023-12-22 16:31:25 +0300

    add logger midleware

--- internal/server/midleware.go
+++ internal/server/midleware.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/Schalure/gofermart/internal/gofermart"
 )
@@ -58,5 +59,87 @@ func (m *Middleware) WithAuthentication(h http.Handler) http.Handler {
 		}
 
 		h.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), contextLoginKey, login)))
+	})
+}
+
+
+type (
+
+	//	Date from response
+	responseData struct {
+		status int
+		size   int
+		data   string
+	}
+
+	//	Response writer with login
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+// ------------------------------------------------------------
+//
+//	Override Write() method by http.ResponseWriter
+//	Receiver:
+//		r *loggingResponseWriter
+//	Input:
+//		b []byte
+//	Output:
+//		int - count of write bytes
+//		err
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+
+	size, err := r.ResponseWriter.Write(b)
+
+	r.responseData.data = string(b)
+	r.responseData.size += size
+	return size, err
+}
+
+// ------------------------------------------------------------
+//
+//	Override WriteHeader() method by http.ResponseWriter
+//	Receiver:
+//		r *loggingResponseWriter
+//	Input:
+//		statusCode int
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
+func (m *Middleware) WithLogging(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		responseData := new(responseData)
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
+		m.logger.Infow("Information about request",
+			"Request URI", r.RequestURI,
+			"Request method", r.Method,
+			"Request headers", r.Header,
+			"Request cookie", r.Cookies(),
+		)
+
+		start := time.Now()
+		h.ServeHTTP(&lw, r)
+		duration := time.Since(start)
+
+		m.logger.Infow(
+			"Information about response",
+			"Response status", responseData.status,
+			"Response headers", lw.ResponseWriter.Header(),
+			"Response cookie", r.Cookies(),
+			"Response data", responseData.data,
+
+			"duration", duration,
+		)
+
 	})
 }
